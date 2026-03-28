7516990b09bd4723069a3f6af9f67b7a7b10546d
Giuseppe Anfuso <giuseppe.anfuso@beyondsports.nl>
2026-03-28 11:25:52 +0100

    Reduce drift

--- Battle Bunny Rescue/Assets/Scripts/Movement/TopDownCarController.cs
+++ Battle Bunny Rescue/Assets/Scripts/Movement/TopDownCarController.cs
@@ -1,28 +1,31 @@
-using System;
 using UnityEngine;
 
 namespace BBR
 {
 	[RequireComponent(typeof(Rigidbody))]
-    public class TopDownCarController : MonoBehaviour
-    {
-        [Header("Car settings")]
-        [SerializeField] private float _accelerationMultiplier = 30.0f;
-         [SerializeField] private float _turnMultiplier = 3.5f;
+	public class TopDownCarController : MonoBehaviour
+	{
+		[Header("Car settings")] [SerializeField]
+		private float _accelerationMultiplier = 30.0f;
+
+		[SerializeField] private float _turnMultiplier = 3.5f;
+		[SerializeField] private float _driftMultiplier = 0.95f;
+		[SerializeField] private float _inPlaceRotationDivider = 8f;
 
 		private float _accelerationInput;
 		private float _steeringInput;
 		private float _rotationAngle;
 		private Rigidbody _rigidbody;
-		
+
 		private void Start()
-        {
-            _rigidbody = GetComponent<Rigidbody>();
-        }
+		{
+			_rigidbody = GetComponent<Rigidbody>();
+		}
 
 		private void FixedUpdate()
 		{
 			ApplyEngineForce();
+			KillOrthogonalVelocity();
 			ApplySteering();
 		}
 
@@ -34,10 +37,29 @@ namespace BBR
 
 		private void ApplySteering()
 		{
-			_rotationAngle += _steeringInput * _turnMultiplier;
+			_rotationAngle += _steeringInput * _turnMultiplier * GetInPlaceDivider();
 			_rigidbody.MoveRotation(Quaternion.AngleAxis(_rotationAngle, Vector3.up));
 		}
-		
+
+		private float GetInPlaceDivider()
+		{
+			if(_inPlaceRotationDivider > 0)
+			{
+				//TODO: Not really working... I don't even think it works
+				float minSpeedTurn = _rigidbody.linearVelocity.magnitude / _inPlaceRotationDivider;
+				return Mathf.Clamp01(minSpeedTurn);
+			}
+			return 1f;
+		}
+
+		private void KillOrthogonalVelocity()
+		{
+			Vector3 forwardVelocity = transform.up * Vector3.Dot(_rigidbody.linearVelocity, transform.up);
+			Vector3 rightVelocity = transform.right * Vector3.Dot(_rigidbody.linearVelocity, transform.right);
+
+			_rigidbody.linearVelocity = forwardVelocity + rightVelocity * _driftMultiplier;
+		}
+
 		public void SetInputVector(Vector2 inputVector)
 		{
 			_steeringInput = inputVector.x;
