f75d7424d85ad6edfd682e9588a131ae1e6326e7
Bella Szabo <bella.szabo@beyondsports.nl>
2026-03-28 12:16:07 +0100

    Added scene utilities to change between scenes and start bootstrap easily

---
+++ Battle Bunny Rescue/Assets/Scripts/Utilities/SceneHelper.cs
@@ -0,0 +1,102 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Project.Utilities
+{
+	/// <summary>
+	/// Used for opening and starting scenes safely.
+	/// </summary>
+	[InitializeOnLoad]
+	internal static class SceneHelper
+	{
+		private const string _bootstrapLoadRequested = "BootstrapLoadRequested";
+		private static string _sceneToOpen;
+		private static bool _shouldStartPlaying;
+
+		static SceneHelper()
+		{
+			EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+		}
+
+		public static void OpenScene(string scene)
+		{
+			if(EditorApplication.isPlaying)
+			{
+				EditorApplication.isPlaying = false;
+			}
+
+			_sceneToOpen = scene;
+			_shouldStartPlaying = false;
+			EditorApplication.update += OnUpdate;
+		}
+
+		public static void StartScene(string scene)
+		{
+			if(EditorApplication.isPlaying)
+			{
+				SaveBootstrapPreference(true);
+				EditorApplication.isPlaying = false;
+			}
+
+			EditorPrefs.SetString("oldScene", SceneManager.GetActiveScene().path);
+			_sceneToOpen = scene;
+			_shouldStartPlaying = true;
+			EditorApplication.update += OnUpdate;
+		}
+
+		private static void OnUpdate()
+		{
+			if(_sceneToOpen == null || EditorApplication.isPlaying || EditorApplication.isPaused || EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode)
+			{
+				return;
+			}
+
+			EditorApplication.update -= OnUpdate;
+
+			if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+			{
+				EditorSceneManager.OpenScene(_sceneToOpen);
+				EditorApplication.isPlaying = _shouldStartPlaying;
+			}
+
+			_sceneToOpen = null;
+		}
+
+		private static void OnPlayModeStateChanged(PlayModeStateChange stateChange)
+		{
+			if(stateChange == PlayModeStateChange.EnteredEditMode)
+			{
+				string oldScene = EditorPrefs.GetString("oldScene");
+
+				if(!string.IsNullOrEmpty(oldScene))
+				{
+					EditorSceneManager.OpenScene(oldScene);
+				}
+
+				EditorPrefs.SetString("oldScene", string.Empty);
+			}
+		}
+
+		private static void SaveBootstrapPreference(bool startBootstrap)
+		{
+			SessionState.SetBool(_bootstrapLoadRequested, startBootstrap);
+		}
+
+		public static bool LoadBootstrapPreference()
+		{
+			if(EditorApplication.isPlaying || EditorApplication.isPaused || EditorApplication.isCompiling)
+			{
+				return false;
+			}
+
+			if(SessionState.GetBool(_bootstrapLoadRequested, false))
+			{
+				SaveBootstrapPreference(false);
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Battle Bunny Rescue/Assets/Scripts/Utilities/SceneUtilities.cs
@@ -0,0 +1,123 @@
+﻿using JetBrains.Annotations;
+using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEditor.Toolbars;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project.Utilities
+{
+	public static class SceneUtilities
+	{
+		private const string _sceneDropdownPath = "Scene Utilities/Scene Selector";
+		private const string _bootstrapPath = "Scene Utilities/Bootstrap";
+
+		private const string _iconPathBootstrapBtn = "Editor/ic_bootstrap";
+		private const string _tooltipBootstrapBtn = "Start the application flow";
+		private const string _tooltipSceneBtn = "Select and open a scene in build settings";
+
+		private static SceneAsset _selectedScene;
+		private static bool _isEnteringPlayModeManually;
+
+		private static string _activeSceneName;
+
+		static SceneUtilities()
+		{
+			SceneManager.activeSceneChanged -= OnActiveSceneChanged;
+			SceneManager.activeSceneChanged += OnActiveSceneChanged;
+			EditorSceneManager.activeSceneChangedInEditMode -= OnActiveSceneChanged;
+			EditorSceneManager.activeSceneChangedInEditMode += OnActiveSceneChanged;
+		}
+
+		[UsedImplicitly]
+		[MainToolbarElement(_sceneDropdownPath, defaultDockPosition = MainToolbarDockPosition.Middle, defaultDockIndex = 1001)]
+		private static MainToolbarElement SceneToolbarGUI()
+		{
+			UpdateActiveSceneName();
+
+			Texture2D icon = EditorGUIUtility.IconContent("UnityLogo").image as Texture2D;
+			MainToolbarContent content = new(_activeSceneName, icon, _tooltipSceneBtn);
+			return new MainToolbarDropdown(content, ShowSceneDropdownMenu);
+		}
+
+		private static void UpdateActiveSceneName()
+		{
+			Scene s = SceneManager.GetActiveScene();
+			_activeSceneName = string.IsNullOrEmpty(s.name) ? "Untitled" : s.name;
+		}
+
+		private static void OnActiveSceneChanged(Scene _, Scene newScene)
+		{
+			_activeSceneName = string.IsNullOrEmpty(newScene.name) ? "Untitled" : newScene.name;
+			MainToolbar.Refresh(_sceneDropdownPath);
+		}
+
+		private static void ShowSceneDropdownMenu(Rect dropDownRect)
+		{
+			GenericMenu menu = new();
+			AddBuilderScenes(menu);
+			menu.DropDown(dropDownRect);
+		}
+
+		private static void AddBuilderScenes(GenericMenu menu)
+		{
+			foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+			{
+				string path = scene.path;
+				string name = GetNameFromPath(path);
+				menu.AddItem(new GUIContent(name), false, () => { SceneHelper.OpenScene(path); });
+			}
+		}
+
+		private static string GetNameFromPath(string scenePath)
+		{
+			int startIndex = scenePath.LastIndexOf("/", StringComparison.Ordinal) + 1;
+			string restString = scenePath[startIndex..];
+			return restString.Remove(restString.IndexOf(".", StringComparison.Ordinal));
+		}
+
+		[UsedImplicitly]
+		[MainToolbarElement(_bootstrapPath, defaultDockPosition = MainToolbarDockPosition.Middle, defaultDockIndex = -1)]
+		private static MainToolbarElement BootstrapToolbarGUI()
+		{
+			Texture2D icon = Resources.Load<Texture2D>(_iconPathBootstrapBtn);
+			MainToolbarContent content = new(icon, _tooltipBootstrapBtn);
+			return new MainToolbarButton(content, StartBootstrapScene);
+		}
+
+		[MenuItem("Scene Utilities/Start Bootstrap %h")]
+		private static void StartBootstrapScene()
+		{
+			SceneHelper.StartScene(GetBootScenePath());
+		}
+
+		private static string GetBootScenePath()
+		{
+			if(EditorBuildSettings.scenes.Length <= 0)
+			{
+				throw new Exception("No scenes are added to this project's build settings");
+			}
+
+			string scenePath = string.Empty;
+
+			foreach(EditorBuildSettingsScene editorBuildSettingsScene in EditorBuildSettings.scenes)
+			{
+				if(!editorBuildSettingsScene.enabled)
+				{
+					continue;
+				}
+
+				scenePath = editorBuildSettingsScene.path;
+				break;
+			}
+
+			if(string.IsNullOrEmpty(scenePath))
+			{
+				throw new Exception("No scenes were enabled in the build settings");
+			}
+
+			return scenePath;
+		}
+	}
+}
