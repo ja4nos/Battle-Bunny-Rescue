de8e7a9434ccf0ce051df124220463ba04f1db6b
Bella Szabo <bella.szabo@beyondsports.nl>
2026-04-04 09:52:14 +0200

    Created assets for player UI

---
+++ Battle Bunny Rescue/Assets/Scripts/Menu/PlayerUIController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Project.Menu
+{
+	public class PlayerUIController : MonoBehaviour
+	{
+		[SerializeField] private UIDocument _uiDocument;
+		[SerializeField] private VisualTreeAsset _playerUIAsset;
+
+		private void Awake()
+		{
+			if(_uiDocument == null)
+			{
+				Debug.LogError($"{nameof(UIDocument)} has been assigned to {nameof(PlayerUIController)}!", this);
+			}
+
+			if(_playerUIAsset == null)
+			{
+				Debug.LogError($"{nameof(VisualTreeAsset)} has been assigned to {nameof(PlayerUIController)}!", this);
+			}
+		}
+
+		public void Init(int playerCount)
+		{
+			for(int i = 0; i < playerCount; i++)
+			{
+				VisualElement playerUIInstance = _playerUIAsset.Instantiate();
+				//playerUIInstance.style.
+			}
+		}
+	}
+}
