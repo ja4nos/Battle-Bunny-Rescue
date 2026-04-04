3a45050e859e67fafa30a92fcac3b89ba7b93d1f
Bella Szabo <bella.szabo@beyondsports.nl>
2026-04-04 14:49:11 +0200

    Added captured bunnies UI

---
+++ Battle Bunny Rescue/Assets/Scripts/Events/CapturedBunniesEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace BBR.Events
+{
+	public class CapturedBunniesEvent
+	{
+		public readonly int PlayerId;
+		public int CapturedBunniesCount;
+
+		public CapturedBunniesEvent(int playerId, int capturedBunniesCount = 0)
+		{
+			PlayerId = playerId;
+			CapturedBunniesCount = capturedBunniesCount;
+		}
+	}
+}
