0a59513cf57ae797fd8cb9a97fc2f68b2fb2a529
Anton Semchenko <anton.semchenko@wearebeyondsports.com>
2026-04-04 11:10:56 +0200

    - camera shake!

---
+++ Battle Bunny Rescue/Assets/Scripts/Events/Camera/CameraShakeEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BBR.Events.Camera
+{
+	public class CameraShakeEvent
+	{
+		public readonly IReadOnlyCollection<int> AffectedPlayers;
+
+		public CameraShakeEvent(IReadOnlyCollection<int> affectedPlayers)
+		{
+			AffectedPlayers = affectedPlayers;
+		}
+	}
+}
